8c83d12e99a9a04b3ed9248a72674a92d9175172
Shagun Bandi <shagunbandi@dev.azure.com>
2020-06-16 23:31:02 +0530

    Updates

---
+++ precache-manifest.950b4c94f9d07ffddd5dccffb512345c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2da100f533c56f7ecc8",
+    "url": "/PhotoAlbumHere/static/js/runtime~main.e2da100f.js"
+  },
+  {
+    "revision": "6c68c44dbfe84c982944",
+    "url": "/PhotoAlbumHere/static/js/main.6c68c44d.chunk.js"
+  },
+  {
+    "revision": "70e15be1aa353fc573a5",
+    "url": "/PhotoAlbumHere/static/js/1.70e15be1.chunk.js"
+  },
+  {
+    "revision": "6c68c44dbfe84c982944",
+    "url": "/PhotoAlbumHere/static/css/main.abf49a4c.chunk.css"
+  },
+  {
+    "revision": "d7f4ab80336cddc45b24937ab10aaf20",
+    "url": "/PhotoAlbumHere/index.html"
+  }
+];
