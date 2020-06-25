8d890d9de072f55c146e02f08edee0aba40c3f68
Shagun Bandi <shagunbandi@dev.azure.com>
2020-06-25 16:16:06 +0530

    album

--- photos/urls.py
+++ photos/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     # path('', views.all_images, name='index'),
     path('api/', views.all_images_urls, name='index_api'),
     path('api/folder', views.get_all_images_with_path, name='index_api_folder'),
-    path('api/album', views.get_all_album_with_path, name='index_api_folder'),
+    path('api/album', views.get_all_album_with_path, name='index_api_album'),
+    path('api/album/images', views.add_images_to_albums, name='add_api_images'),
+    path('api/album/images/delete', views.delete_images_from_albums, name='add_api_images'),
 ]
 
--- photos/views.py
+++ photos/views.py
@@ -19,6 +19,64 @@ def get_all_images_with_path(request):
 
 
 @csrf_exempt
+def add_images_to_albums(request):
+    try:
+        with open('album.albumify') as json_file:
+            data = json.load(json_file)
+    except:
+        data = {
+            "response": "Success",
+            "data": {
+                "file": {},
+                "folder": {},
+                "root": "home"
+            }
+        }
+    payload = json.loads(request.body)
+    album_path = payload['album_path']
+    file_paths = payload['file_paths']
+
+    if request.method == 'POST':
+        if album_path in data['data']['file']:
+            data['data']['file'][album_path] += file_paths
+        else:
+            data['data']['file'][album_path] = file_paths
+        data['data']['file'][album_path] = list(set(data['data']['file'][album_path]))
+
+    with open("album.albumify", "w") as write_file:
+        json.dump(data, write_file)
+
+    return JsonResponse(data)
+
+
+@csrf_exempt
+def delete_images_from_albums(request):
+    try:
+        with open('album.albumify') as json_file:
+            data = json.load(json_file)
+    except:
+        data = {
+            "response": "Success",
+            "data": {
+                "file": {},
+                "folder": {},
+                "root": "home"
+            }
+        }
+    payload = json.loads(request.body)
+    album_path = payload['album_path']
+    file_paths = payload['file_paths']
+
+    if request.method == 'POST':
+        data['data']['file'][album_path] = list(set(data['data']['file'][album_path]) - set(file_paths))
+
+    with open("album.albumify", "w") as write_file:
+        json.dump(data, write_file)
+
+    return JsonResponse(data)
+
+
+@csrf_exempt
 def get_all_album_with_path(request):
     try:
         with open('album.albumify') as json_file:
@@ -56,7 +114,6 @@ def get_all_album_with_path(request):
 
     data['current_directory'] = album_path
     return JsonResponse(data)
-
 
 
 def all_images_urls(request):
